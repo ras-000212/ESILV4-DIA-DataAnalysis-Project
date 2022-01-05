7f7a0fb4f816057bb1c61a35b0d7e3e1413f2ab3
ArthurVinot <vinot.arthur@outlook.com>
2022-01-05 23:48:55 +0100

    Flask Update V2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
     return flask.render_template("index.html")
 
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,4)
+    to_predict = np.array(to_predict_list).reshape(1,12)
     loaded_model = pickle.load(open("model.pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
